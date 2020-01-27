ba01f3adecca9e22062cb81e80d5b6c99b506c45
Pavel Urban <58266541+browser091@users.noreply.github.com>
2020-01-27 14:32:14 +0300

    cannit change

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-gen = function () {
+function gen() {
     return Math.round(Math.random() * 100)
 }
-
-arr = []
-
-inArr = function (val) {
-    for (var i = 0; i <= arr.length; i++) {
+let arr = []
+function inArr(val) {
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] === val) return true
     }
 }
-
 while (arr.length < 10) {
-    tmp = gen()
+    let tmp = gen()
     if (inArr(tmp)) continue;
     arr.push(tmp)
 }
 arr = arr.join()
-arr = arr.replace(/0/i, 'zero');
-arr = arr.split(',', 10);
-console.log(arr);
+let arrOut ='';
+for (let i = 0; i < arr.length; i++){
+    let outputSign = arr[i];  
+    if (outputSign == "0"){
+        arrOut += "zero";}
+    else {
+        arrOut += outputSign;
+    }}
+arrOut = arrOut.split(',', 10);
+console.log(arrOut);
